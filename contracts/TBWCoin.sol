d313b9aa4907f1cc44bdf679a95f5d5ceb345e6b
ALBERT <thatguyintech@gmail.com>
2022-12-09 16:48:53 +0800

    deploy TBWCoin erc20

---
+++ contracts/TBWCoin.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.9;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract TBWCoin is ERC20, Ownable {
+    constructor() ERC20("TaipeiBlockchainWeekCoin", "TBW") {
+        // pre-mint one million coins to the deployer address
+        _mint(msg.sender, 1_000_000 * 10 ** decimals());
+    }
+
+    function mint(address to, uint256 amount) public onlyOwner {
+        _mint(to, amount);
+    }
+}
--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,18 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import * as dotenv from "dotenv";
+dotenv.config();
 
 const config: HardhatUserConfig = {
   solidity: "0.8.9",
+  networks: {
+    goerli: {
+      url: process.env.ALCHEMY_API_URL,
+      accounts: [ process.env.PRIVATE_KEY! ],
+      blockGasLimit: 10_000_000,
+    }
+  },
+  defaultNetwork: "goerli",
 };
 
 export default config;
